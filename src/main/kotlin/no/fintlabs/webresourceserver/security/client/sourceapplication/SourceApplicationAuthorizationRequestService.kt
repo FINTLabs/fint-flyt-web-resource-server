bc8387c86e0184c3eaf8f67bd24351bf9f2109f8
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-09-04 11:37:30 +0200

    bump to fint-kafka:5.0.0-rc-8
    Fix code in `SourceApplicationAuthorizationRequestService` to use request/reply.

--- src/main/kotlin/no/fintlabs/webresourceserver/security/client/sourceapplication/SourceApplicationAuthorizationRequestService.kt
+++ src/main/kotlin/no/fintlabs/webresourceserver/security/client/sourceapplication/SourceApplicationAuthorizationRequestService.kt
@@ -1,53 +1,87 @@
 package no.fintlabs.webresourceserver.security.client.sourceapplication
 
+import no.fintlabs.kafka.consuming.ErrorHandlerConfiguration
+import no.fintlabs.kafka.consuming.ListenerConfiguration
+import no.fintlabs.kafka.requestreply.RequestProducerRecord
+import no.fintlabs.kafka.requestreply.RequestTemplate
+import no.fintlabs.kafka.requestreply.RequestTemplateFactory
+import no.fintlabs.kafka.requestreply.topic.ReplyTopicService
+import no.fintlabs.kafka.requestreply.topic.configuration.ReplyTopicConfiguration
+import no.fintlabs.kafka.requestreply.topic.name.ReplyTopicNameParameters
+import no.fintlabs.kafka.requestreply.topic.name.RequestTopicNameParameters
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.Optional
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 @Service
 class SourceApplicationAuthorizationRequestService(
-    @Value("\${fint.kafka.application-id}") applicationId: String,
-//    requestProducerFactory: RequestProducerFactory,
-//    replyTopicService: ReplyTopicService,
+    @Value("\${fint.kafka.application-id}") private val applicationId: String,
+    @Value("\${fint.kafka.reply-topic.retention-hours:1}") private val replyRetentionHours: Long,
+    @Value("\${fint.kafka.reply-topic.request-timeout-seconds:30}") private val requestTimeoutSeconds: Long,
+    requestTemplateFactory: RequestTemplateFactory,
+    replyTopicService: ReplyTopicService,
 ) {
-//    private val requestTopicNameParameters: RequestTopicNameParameters =
-//        RequestTopicNameParameters
-//            .builder()
-//            .resource("authorization")
-//            .parameterName("client-id")
-//            .build()
-//
-//    private lateinit var requestProducer: RequestProducer<String, SourceApplicationAuthorization>
-//
-//    init {
-//        val replyTopicNameParameters =
-//            ReplyTopicNameParameters
-//                .builder()
-//                .applicationId(applicationId)
-//                .resource("authorization")
-//                .build()
-//        replyTopicService.ensureTopic(
-//            replyTopicNameParameters,
-//            0,
-//            TopicCleanupPolicyParameters.builder().build(),
-//        )
-//        requestProducer =
-//            requestProducerFactory.createProducer(
-//                replyTopicNameParameters,
-//                String::class.java,
-//                SourceApplicationAuthorization::class.java,
-//            )
-//    }
+    companion object {
+        private const val RESOURCE_NAME: String = "authorization"
+        private const val PARAMETER_NAME: String = "client-id"
+    }
+
+    private val requestTopicNameParameters: RequestTopicNameParameters =
+        RequestTopicNameParameters
+            .builder()
+            .resourceName(RESOURCE_NAME)
+            .parameterName(PARAMETER_NAME)
+            .build()
+
+    private val requestTemplate: RequestTemplate<String, SourceApplicationAuthorization>
+
+    init {
+        val replyTopicNameParameters =
+            ReplyTopicNameParameters
+                .builder()
+                .applicationId(applicationId)
+                .resourceName(RESOURCE_NAME)
+                .build()
+
+        replyTopicService.createOrModifyTopic(
+            replyTopicNameParameters,
+            ReplyTopicConfiguration
+                .builder()
+                .retentionTime(replyRetentionHours.hours.toJavaDuration())
+                .build(),
+        )
+
+        requestTemplate =
+            requestTemplateFactory.createTemplate(
+                replyTopicNameParameters,
+                String::class.java,
+                requestTimeoutSeconds.seconds.toJavaDuration(),
+                ListenerConfiguration
+                    .builder(SourceApplicationAuthorization::class.java)
+                    .groupIdApplicationDefault()
+                    .maxPollRecordsKafkaDefault()
+                    .maxPollIntervalKafkaDefault()
+                    .errorHandler(
+                        ErrorHandlerConfiguration
+                            .builder(SourceApplicationAuthorization::class.java)
+                            .noRetries()
+                            .skipFailedRecords()
+                            .build(),
+                    ).continueFromPreviousOffsetOnAssignment()
+                    .build(),
+            )
+    }
 
     fun getClientAuthorization(clientId: String): SourceApplicationAuthorization? {
-//        return requestProducer
-//            .requestAndReceive(
-//                RequestProducerRecord
-//                    .builder<String>()
-//                    .topicNameParameters(requestTopicNameParameters)
-//                    .value(clientId)
-//                    .build(),
-//            ).map(ConsumerRecord<String, SourceApplicationAuthorization>::value)
-        return TODO("Provide the return value")
+        return requestTemplate
+            .requestAndReceive(
+                RequestProducerRecord(
+                    requestTopicNameParameters,
+                    clientId,
+                    clientId,
+                ),
+            ).value()
     }
 }
