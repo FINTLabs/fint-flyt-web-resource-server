e8a4eb048a9cd49a46c554f17cdffe9f251ea72e
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-09-12 08:56:25 +0200

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
-    id("org.springframework.boot") version "3.4.5"
+    id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("maven-publish")
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.spring") version "2.1.10"
-    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
 }
 
 group = "no.fintlabs"
@@ -36,7 +36,7 @@ dependencies {
     implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
     implementation("jakarta.servlet:jakarta.servlet-api:6.1.0")
 
-    implementation("no.fintlabs:fint-flyt-cache:1.2.3")
+    implementation("no.fintlabs:fint-flyt-cache:1.2.5")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("no.fintlabs:fint-kafka:5.0.0-rc-8")
 
@@ -44,7 +44,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.0.0")
 }
 
 tasks.test {
